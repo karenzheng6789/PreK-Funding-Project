452a66511205067bf4e3d9d6b70bb320574fa4f4
Karen Zheng <Zheka-26@rhodes.edu>
2025-11-09 15:32:12 -0600

    change to pandas method

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import csv
+import pandas as pd
 
 from sqlalchemy.sql.sqltypes import NULLTYPE
 
-preK_names = []
-license_type = []
-three_year_old_capacity = []
-four_year_old_capacity = []
-total_capacity = []
-test = []
-grade_and_subject = []
-valid_tests = []
-proficient_tests = []
-percent_proficient = []
-funding_source = []
-city_council_district = []
-commission_district = []
-super_district = []
+df = pd.read_csv("prek_capacity_2024.csv", delimiter=';')
+print(df.head())
 
-with open("prek_capacity_2024.csv", "r", newline="") as prek_file:
-    for line in prek_file:
-        line.strip()
-        parts = line.split(";")
-        print(parts)
-        for i in parts:
-            #if parts[i] != "-" :
-            preK_names.append(parts[0])
+preK_names = df["Prek Center Name"].tolist()
+license_type = df["License Type"].tolist()
+three_year_old_capacity = df["3 Year Old Capacity"].tolist()
+four_year_old_capacity = df["4 Year Old Capacity"].tolist()
+total_capacity = df["Total Capacity"].tolist()
+test_type = df["Test"].tolist()
+valid_tests = df["Valid Tests"].tolist()
+proficient_tests = df["Proficient Tests"].tolist()
+percent_proficient = df["Percent Proficient"].tolist()
+funding_source = df["Funding Source"].tolist()
+city_council_district = df["City Council District"].tolist()
+commission_district = df["Commission District"].tolist()
+super_district = df["Super District"].tolist()
 
+total_capacity = pd.to_numeric(df["Total Capacity"])
 
